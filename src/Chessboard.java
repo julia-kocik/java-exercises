d04ea93576f5eeffb8e2e58df2a8dfe03c19c23e
julia-kocik <julia.kocik@gmail.com>
2024-12-11 09:05:15 +0100

    Add Chessboard.java

---
+++ src/Chessboard.java
@@ -0,0 +1,19 @@
+public class Chessboard {
+    public static void main(String[] args) {
+        int n = 8;
+        int[][] board = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
